5716978aee02913cdf6381fc1565564fa32e4cd8
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-01-18 15:10:25 -0800

    Added documentation to the Instructor class

--- NestedObjects/Instructor.cs
+++ NestedObjects/Instructor.cs
@@ -8,10 +8,21 @@ namespace NestedObjects
 {
     class Instructor
     {
+        /// <summary>
+        /// The Instructors First and Last name
+        /// ex. Eliseo Rocha
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The Instructors work email
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// The office phone number for the
+        /// Instructor
+        /// </summary>
         public string PhoneNumber { get; set; }
 
 
